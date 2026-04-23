f4b4c38b352f7dff3df37258dd3d589c25f5667b
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-23 15:15:17 -0300

    [REFACTOR]Agrego excepciones a Partido

--- WorldCupPlanner/Dominio/Clases/Equipo.cs
+++ WorldCupPlanner/Dominio/Clases/Equipo.cs
@@ -1,6 +1,16 @@
 namespace Dominio.Clases;
+using Dominio.Enums;
 
 public class Equipo
 {
+    public string Nombre { get; }
+    public Confederacion Confederacion { get; }
+    public int RankingFifa { get; }
 
+    public Equipo(string nombre, Confederacion confederacion, int rankingFifa)
+    {
+        Nombre = nombre;
+        Confederacion = confederacion;
+        RankingFifa = rankingFifa;
+    }
 }
--- WorldCupPlanner/Dominio/Clases/Estadio.cs
+++ WorldCupPlanner/Dominio/Clases/Estadio.cs
@@ -2,5 +2,14 @@ namespace Dominio.Clases;
 
 public class Estadio
 {
+    public string Nombre { get; }
+    public string Ciudad { get; }
+    public int Capacidad { get; }
 
+    public Estadio(string nombre, string ciudad, int capacidad)
+    {
+        Nombre = nombre;
+        Ciudad = ciudad;
+        Capacidad = capacidad;
+    }
 }
