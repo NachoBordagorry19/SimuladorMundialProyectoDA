6e2c9db3713736c39be29afe11942f577c75e75d
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-23 09:55:29 -0300

    [RED] test RegistrarResultado en Partido

--- WorldCupPlanner/TestDominio/Clases/TestsPartido.cs
+++ WorldCupPlanner/TestDominio/Clases/TestsPartido.cs
@@ -1,6 +1,25 @@
+using NUnit.Framework;
+using Dominio.Clases;
+using Dominio.Enums;
+
 namespace TestDominio.Clases;
 
+[TestFixture]
 public class TestsPartido
 {
-    
+    [Test]
+    public void RegistrarResultado_DeberiaAsignarGolesYEstadoJugado()
+    {
+        var equipo1 = new Equipo("Uruguay", Confederacion.CONMEBOL, 1);
+        var equipo2 = new Equipo("Brasil", Confederacion.CONMEBOL, 2);
+        var estadio = new Estadio("Azteca", "Ciudad de Mexico", 83000);
+
+        var partido = new Partido(equipo1, equipo2, estadio);
+
+        partido.RegistrarResultado(2, 1);
+
+        Assert.AreEqual(2, partido.GolesLocal);
+        Assert.AreEqual(1, partido.GolesVisitante);
+        Assert.AreEqual(EstadoPartido.Jugado, partido.Estado);
+    }
 }
