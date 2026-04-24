ddbae3ce85ca6f41ee67cfb080d4c7152d2c04c9
Nacho <nachobordagorry2020@gmail.com>
2026-04-23 22:50:25 -0300

    [GREEN] agrego codigo suficiente para que la prueba de contraseña sin minuscula pase

--- WorldCupPlanner/Dominio/Clases/Usuario.cs
+++ WorldCupPlanner/Dominio/Clases/Usuario.cs
@@ -84,6 +84,9 @@ public class Usuario
             else if (!value.Any(char.IsUpper))
             {
                 throw new ArgumentException("Contraseña debe contener al menos una letra mayúscula");
+            } else if (!value.Any(char.IsLower))
+            {
+                throw new ArgumentException("La contraseña debe contener una letra minusucla");
             }
             _contraseña = value;
         } 
