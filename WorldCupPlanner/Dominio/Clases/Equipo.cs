c6bc6d7ee2d1b0361a5ed24ad10f53d68d6951cf
Nacho <nachobordagorry2020@gmail.com>
2026-04-23 23:35:56 -0300

    [GREEN] agrego codigo para que la prueba de nombre vacio pase

--- WorldCupPlanner/Dominio/Clases/Equipo.cs
+++ WorldCupPlanner/Dominio/Clases/Equipo.cs
@@ -5,9 +5,32 @@ public class Equipo
     private string _nombre;
     private string _confederacion;
     private int _rankingFifa;
+
+    public String Nombre
+    {
+        get => _nombre;
+        set
+        {
+            if (EsVacio(value))
+            {
+                throw new ArgumentException("El nombre del equipo no puede ser vacio");
+            }
+            _nombre = value;
+        }
+    }
+    
+    public bool EsVacio(string textoATestear)
+    {
+        bool vacio = true;
+        if (!String.IsNullOrEmpty(textoATestear))
+        {
+            vacio = false;
+        }
+        return vacio;
+    }
     public Equipo(string nombre, string condeferacion, int rankingFifa)
     {
-        _nombre = nombre;
+        Nombre = nombre;
         _confederacion = condeferacion;
         _rankingFifa = rankingFifa;
     }
