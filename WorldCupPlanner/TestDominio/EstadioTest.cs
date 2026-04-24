a9340b1cca4422690bffe47a548ca40e7505b15d
Nacho <nachobordagorry2020@gmail.com>
2026-04-24 00:25:33 -0300

    [RED] hago prueba para ingresar estadio sin hombre

--- WorldCupPlanner/TestDominio/EstadioTest.cs
+++ WorldCupPlanner/TestDominio/EstadioTest.cs
@@ -10,4 +10,11 @@ public class EstadioTest
     {
         Estadio estadioPrueba = new Estadio("Allianz Arena", "Munich", "El mejor estadio del mundo", 75024);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreoEstadio_SinNombre_LanzoExcepcion()
+    {
+        Estadio estadioPrueba = new Estadio("", "Munich", "El mejor estadio del mundo", 75024);
+    }
 }
