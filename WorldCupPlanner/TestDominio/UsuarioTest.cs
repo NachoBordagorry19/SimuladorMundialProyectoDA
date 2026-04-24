89fc91bac270b30161b272ab884bb6f68269ff8f
Nacho <nachobordagorry2020@gmail.com>
2026-04-23 22:58:38 -0300

    [REFACTOR] mejoro nombres de variables en test

--- WorldCupPlanner/TestDominio/UsuarioTest.cs
+++ WorldCupPlanner/TestDominio/UsuarioTest.cs
@@ -24,53 +24,53 @@ public class UsuarioTest
     [ExpectedException(typeof(ArgumentException))]
     public void NuevoUsuario_SiApellidoEsNulo_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "", "a@gmail.com", new DateTime(2005, 05 ,20), "23456abbbbAA.");
+        Usuario usuarioPrueba = new Usuario("Federico", "", "a@gmail.com", new DateTime(2005, 05 ,20), "23456abbbbAA.");
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void InsertoEmail_SiEmailEsVacio_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "", new DateTime(2005 , 05 , 20), "23456abbbbAA.");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "", new DateTime(2005 , 05 , 20), "23456abbbbAA.");
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void InsertoFecha_SiFechaVieneVacia_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "a@gmail.com", DateTime.MinValue, "23456abbbbAA.");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", DateTime.MinValue, "23456abbbbAA.");
     }
 
     [TestMethod] [ExpectedException(typeof(ArgumentException))]
     public void InsertoContraseña_SiContraseñaEsVacia_TiroExcepcion()
     {
-        Usuario usuario = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 03 , 20), "");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 03 , 20), "");
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void InsertoContraseña_SiContraseñaNoTieneMayuscula_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "asdfg23455.");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "asdfg23455.");
     }
     
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void InsertoContraseña_SiContraseñaNoTieneMinuscula_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "ASDFGDS2344.");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "ASDFGDS2344.");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void InsertoContraseña_SiContraseñaNoTieneNumero_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "AASDSAasddsa.");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "AASDSAasddsa.");
     }
 
     [TestMethod] [ExpectedException(typeof(ArgumentException))]
     public void InsertoContraseña_SiContraseñaNoTieneCaracterEspecial_TiroExcepcion()
     {
-        Usuario user = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "ADDSSSDSAsdsa23");
+        Usuario usuarioPrueba = new Usuario("Federico", "Gonzalez", "a@gmail.com", new DateTime(2005 , 05 , 20), "ADDSSSDSAsdsa23");
     }
 }
