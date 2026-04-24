0e42606a2ebd336e946a24f68ed2ef24c02257da
Nacho <nachobordagorry2020@gmail.com>
2026-04-24 00:28:05 -0300

    [GREEN] hago codigo para que cuando se agregue un estadio sin nombre se lance excepcion

--- WorldCupPlanner/Dominio/Clases/Estadio.cs
+++ WorldCupPlanner/Dominio/Clases/Estadio.cs
@@ -6,10 +6,32 @@ public class Estadio
     private string _ciudad;
     private string _descripcion;
     private int _capacidadLocativa;
+
+    public string Nombre
+    {
+        get => _nombre;
+        set
+        {
+            if (EsVacio(value))
+            {
+                throw new ArgumentException("El nombre no puede ser vacio");
+            }
+            _nombre = value;
+        }
+    }
     
+    public bool EsVacio(string textoATestear)
+    {
+        bool vacio = true;
+        if (!String.IsNullOrEmpty(textoATestear))
+        {
+            vacio = false;
+        }
+        return vacio;
+    }
     public Estadio(string nombre, string ciudad, string descripcion, int capacidadLocativa)
     {
-        _nombre = nombre;
+        Nombre = nombre;
         _ciudad = ciudad;
         _descripcion = descripcion;
         _capacidadLocativa = capacidadLocativa;
