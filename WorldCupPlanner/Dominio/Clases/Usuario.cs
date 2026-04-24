c9128b43bbf3d9d756817e87aecb4c8220436b93
Nacho <nachobordagorry2020@gmail.com>
2026-04-23 22:53:27 -0300

    [GREEN] Agrego codigo para que contraseña sin numero pase

--- WorldCupPlanner/Dominio/Clases/Usuario.cs
+++ WorldCupPlanner/Dominio/Clases/Usuario.cs
@@ -87,7 +87,8 @@ public class Usuario
             } else if (!value.Any(char.IsLower))
             {
                 throw new ArgumentException("La contraseña debe contener una letra minusucla");
-            }
+            } else if (!value.Any(char.IsDigit))
+                throw new ArgumentException("Contraseña debe contener al menos un número");
             _contraseña = value;
         } 
     }
