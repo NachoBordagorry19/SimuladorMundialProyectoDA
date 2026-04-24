8dfa25066fecc40d0493a17b0e2d15e144cd19cc
Nacho <nachobordagorry2020@gmail.com>
2026-04-24 00:33:06 -0300

    [RED] creo prueba para que la ciudad no pueda venir vacia

--- WorldCupPlanner/TestDominio/EstadioTest.cs
+++ WorldCupPlanner/TestDominio/EstadioTest.cs
@@ -24,4 +24,11 @@ public class EstadioTest
     {
         Estadio estadioPrueba = new Estadio("Nombre con longitud excesiva para pruebas unitarias del sistema (81 caracteres)!!", "Munich", "El mejor estadio del mundo", 75024);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreoEstadio_SinCiudad_LanzoExcepcion()
+    {
+        Estadio estadioPrueba = new Estadio("Allianz Arena", "", "El mejor estadio del mundo", 75024);
+    }
 }
