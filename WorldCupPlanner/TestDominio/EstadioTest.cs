400f3f7a331d7c87b6aa1c18fdbaa5e2e5be78ba
Nacho <nachobordagorry2020@gmail.com>
2026-04-24 00:30:45 -0300

    [RED] hago prueba para que el nombre del estadio no supere los 80 caracteres

--- WorldCupPlanner/TestDominio/EstadioTest.cs
+++ WorldCupPlanner/TestDominio/EstadioTest.cs
@@ -17,4 +17,11 @@ public class EstadioTest
     {
         Estadio estadioPrueba = new Estadio("", "Munich", "El mejor estadio del mundo", 75024);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreoEstadio_MayorA80Caracteres_LanzoExcepcion()
+    {
+        Estadio estadioPrueba = new Estadio("Nombre con longitud excesiva para pruebas unitarias del sistema (81 caracteres)!!", "Munich", "El mejor estadio del mundo", 75024);
+    }
 }
