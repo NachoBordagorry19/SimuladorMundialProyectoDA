d02ba5dc5dd9e19960421328e2f1e0adc9fc86d0
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-25 20:14:04 -0300

    [GREEN] Implemento exepcion NombreEntreAyL en Grupo

--- WorldCupPlanner/Dominio/Clases/Grupo.cs
+++ WorldCupPlanner/Dominio/Clases/Grupo.cs
@@ -13,6 +13,11 @@ public class Grupo
             {
                 throw new ArgumentException("El nombre del grupo no puede ser vacio");
             }
+            
+            if(value.Length != 1 || value[0] < 'A' || value[0] > 'L')
+            {
+                throw new ArgumentException("El nombre del grupo debe ser una letra entre A y L");
+            }
             _nombre = value;   
         }
     }
