9b189309eed92c09ec2901165ef4459c4951cef1
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-25 18:54:05 -0300

    [GREEN] implementacion minima de Grupo

--- WorldCupPlanner/Dominio/Clases/Grupo.cs
+++ WorldCupPlanner/Dominio/Clases/Grupo.cs
@@ -2,5 +2,16 @@ namespace Dominio.Clases;
 
 public class Grupo
 {
+    private string _nombre;
 
+    public string Nombre
+    {
+        get => _nombre;
+        set => _nombre = value;
+    }
+    
+    public Grupo(string nombre)
+    {
+        Nombre = nombre;
+    }
 }
