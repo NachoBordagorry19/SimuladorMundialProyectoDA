69626a9e87a117be4b7c9ceed188f915e4bb6c88
Nacho <ignaciorubino14@gmail.com>
2026-04-26 18:40:44 -0300

    [RED] ObtenerEstadios_CuandoNoHayEstadios_RetornaListaVacia

--- WorldCupPlanner/TestRepositorio/EstadioRepositorioTest.cs
+++ WorldCupPlanner/TestRepositorio/EstadioRepositorioTest.cs
@@ -1,3 +1,4 @@
+using Dominio.Clases;
 using Repositorio;
 
 namespace TestRepositorio;
@@ -5,8 +6,8 @@ namespace TestRepositorio;
 [TestClass]
 public class EstadioRepositorioTest
 {
-    private BaseDeDatosEnMemoria BDEnMemoria;
-    private EstadioRepositorio _repositorioEstadio;
+    private BaseDeDatosEnMemoria BDEnMemoria = null;
+    private EstadioRepositorio _repositorioEstadio = null;
 
     [TestInitialize]
     public void Inicializar()
@@ -14,5 +15,21 @@ public class EstadioRepositorioTest
         BDEnMemoria = new BaseDeDatosEnMemoria();
         _repositorioEstadio = new EstadioRepositorio(BDEnMemoria);
     }
+
+    private static Estadio CrearEstadio(
+        string nombre = "Centenario",
+        string ciudad = "Montevideo",
+        string descripcion = "descripcion",
+        int? capacidadLocativa = 50000)
+    {
+        return new Estadio(nombre, ciudad, descripcion, capacidadLocativa);
+    }
     
+    [TestMethod]
+    public void ObtenerEstadios_CuandoNoHayEstadios_RetornaListaVacia()
+    {
+        var estadiosPrueba = _repositorioEstadio.ObtenerEstadios();
+        Assert.IsNotNull(estadiosPrueba);
+        Assert.AreEqual(0,estadiosPrueba.Count);
+    }
 }
