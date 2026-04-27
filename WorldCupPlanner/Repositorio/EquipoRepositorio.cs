7dfe5c43bdfb31877d8f9a06c500f087aa06a45f
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-27 00:13:34 -0300

    [GREEN] Implemento obtener Equipos/EquiposVacios

--- WorldCupPlanner/Repositorio/EquipoRepositorio.cs
+++ WorldCupPlanner/Repositorio/EquipoRepositorio.cs
@@ -4,5 +4,15 @@ namespace Repositorio;
 
 public class EquipoRepositorio
 {
-    
+    private BaseDeDatosEnMemoria _baseDeDatosEnMemoria;
+
+    public EquipoRepositorio(BaseDeDatosEnMemoria baseDeDatosEnMemoria)
+    {
+        _baseDeDatosEnMemoria = baseDeDatosEnMemoria;
+    }
+
+    public List<Equipo> ObtenerEquipos()
+    {
+        return _baseDeDatosEnMemoria.ObtenerEquipos();
+    }
 }
