21c787e24d144361553ed078c9313f34e3de282b
Nacho <ignaciorubino14@gmail.com>
2026-04-26 21:00:03 -0300

    [GREEN] Se modifica AgregarEstadio en EstadioRepositorio para manejar Exception de estadios duplicados

--- WorldCupPlanner/Repositorio/EstadioRepositorio.cs
+++ WorldCupPlanner/Repositorio/EstadioRepositorio.cs
@@ -18,6 +18,11 @@ public class EstadioRepositorio
 
     public void AgregarEstadio(Estadio estadio)
     {
+        var existe =  _BDEnMemoria.ObtenerEstadios().Any(e => e.Nombre.Equals(estadio.Nombre, StringComparison.OrdinalIgnoreCase));
+        if (existe)
+        {
+            throw new InvalidOperationException("Ya existe un estadio con el nombre: " + estadio.Nombre);
+        }
         _BDEnMemoria.AgregarEstadio(estadio);
     }
 
