8e990821d432ed567be5351518a6165f360136b5
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-28 11:51:10 -0300

    Hago IEstadioRepositorio

--- WorldCupPlanner/Repositorio/Interfaces/IEstadioRepositorio.cs
+++ WorldCupPlanner/Repositorio/Interfaces/IEstadioRepositorio.cs
@@ -4,5 +4,9 @@ namespace Repositorio.Interfaces;
 
 public interface IEstadioRepositorio
 {
-    
+    List<Estadio> ObtenerEstadios();
+    void AgregarEstadio(Estadio estadio);
+    Estadio? ObtenerEstadioPorNombre(string nombre);
+    bool ActualizarEstadio(Estadio estadio);
+    void EliminarEstadio(Estadio estadio);
 }
