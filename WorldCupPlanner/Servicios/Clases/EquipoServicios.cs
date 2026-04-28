d4291db565b1717fd396ce27e49eb51ba4e19ac2
Nacho <nachobordagorry2020@gmail.com>
2026-04-28 09:17:32 -0300

    [GREEN] agrego codigo suficiente para que el inizializador funcione

--- WorldCupPlanner/Servicios/Clases/EquipoServicios.cs
+++ WorldCupPlanner/Servicios/Clases/EquipoServicios.cs
@@ -1,6 +1,15 @@
+using Repositorio.Interfaces;
+
 namespace Servicios.Clases;
 
 public class EquipoServicios
 {
+    private readonly IEquipoRepositorio _equipoRepositorio;
+
+    public EquipoServicios(IEquipoRepositorio equipoRepositorio)
+    {
+        _equipoRepositorio = equipoRepositorio;
+    }
+    
     
 }
