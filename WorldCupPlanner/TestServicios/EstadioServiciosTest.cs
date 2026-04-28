9a21b9fda17cd472196b3932cbf2c2bc193d5469
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-28 13:41:37 -0300

    Creo test Iniciar y DTO

--- WorldCupPlanner/TestServicios/EstadioServiciosTest.cs
+++ WorldCupPlanner/TestServicios/EstadioServiciosTest.cs
@@ -1,6 +1,32 @@
+using Repositorio;
+using Repositorio.Interfaces;
+using Servicios.Interfaces;
+using Servicios.Modelo;
+using Servicios.Clases;
+
 namespace TestServicios;
 
+[TestClass]
 public class EstadioServiciosTest
 {
-    
+    private BaseDeDatosEnMemoria _baseDeDatosEnMemoria;
+    private IEstadioRepositorio _estadioRepositorio;
+    private IServicioEstadio _servicioEstadio;
+    private EstadioDTO _estadioDTO;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _baseDeDatosEnMemoria = new BaseDeDatosEnMemoria();
+        _estadioRepositorio = new EstadioRepositorio(_baseDeDatosEnMemoria);
+        _servicioEstadio = new ServicioEstadio(_estadioRepositorio);
+        
+        _estadioDTO = new EstadioDTO()
+        {
+            Nombre = "CDS",
+            Ciudad = "Montevideo",
+            Descripcion = "descripcion",
+            CapacidadLocativa = 50000,
+        };
+    }
 }
