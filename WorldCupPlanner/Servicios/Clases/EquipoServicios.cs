54a6168657051be0b2eb539efbb651411422f38b
Nacho <nachobordagorry2020@gmail.com>
2026-04-30 10:39:11 -0300

    [GREEN] agrego codigo suficiente para que la prueba de verificar si el equipo no existe lace excepcion funcione

--- WorldCupPlanner/Servicios/Clases/EquipoServicios.cs
+++ WorldCupPlanner/Servicios/Clases/EquipoServicios.cs
@@ -58,12 +58,18 @@ public class EquipoServicios
 
     public EquipoDTO ObtenerEquipo(string nombre)
     {
+        ValidarNombre(nombre);
         Equipo? equipo = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == nombre);
-        if (equipo == null)
-        {
-            throw new ArgumentException($"No se encontró el equipo con nombre: {nombre}");
-        }
         return EquipoEntidadAEquipoDTO(equipo);
+    }
+
+    public void ValidarNombre(string nombre)
+    {
+        Equipo equipoExistente = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == nombre);
+        if (equipoExistente == null)
+        {
+            throw new ArgumentException("El equipo a buscar no existe por favor ingrese uno que exista");
+        }
     }
 
 }
