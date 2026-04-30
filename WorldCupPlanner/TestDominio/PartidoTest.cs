0dacf5f8d647059b121c2f4ff88f4a1b63c3b6a0
Fabrizio Perotti <fperotti2323@gmail.com>
2026-04-30 19:21:25 -0300

    [RED] Tests fecha invalida

--- WorldCupPlanner/TestDominio/PartidoTest.cs
+++ WorldCupPlanner/TestDominio/PartidoTest.cs
@@ -24,4 +24,28 @@ public class PartidoTest
         Assert.AreEqual(0, partido.GolesLocal);
         Assert.AreEqual(0, partido.GolesVisitante);
     }
+    
+    [TestMethod]
+    public void Partido_IdEsIncremental()
+    {
+        Equipo local = new Equipo("A", Confederacion.CONMEBOL, 1000);
+        Equipo visitante = new Equipo("B", Confederacion.UEFA, 1100);
+        Estadio estadio = new Estadio("Campeon del Siglo", "Montevideo", "Decano", 60000);
+
+        Partido p1 = new Partido(DateTime.Now, estadio, local, visitante, Fase.Grupos, 0, 0);
+        Partido p2 = new Partido(DateTime.Now, estadio, local, visitante, Fase.Grupos, 0, 0);
+
+        Assert.AreEqual(p1.Id + 1, p2.Id);
+    }
+    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Partido_FechaNoPuedeSerInvalida_TiraExcepcion()
+    {
+        Equipo local = new Equipo("A", Confederacion.CONMEBOL, 1000);
+        Equipo visitante = new Equipo("B", Confederacion.UEFA, 1200);
+        Estadio estadio = new Estadio("Estadio", "Ciudad", "Descripcion", 30000);
+
+        new Partido(DateTime.MinValue, estadio, local, visitante, Fase.Grupos, 0, 0);
+    }
 }
