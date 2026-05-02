e5791de306cfc088a14b13a2c5e65d4b6981e8a8
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-02 16:29:30 -0300

    [RED] Test obtener lista partidos vacios

--- WorldCupPlanner/TestRepositorio/PartidoRepositorioTest.cs
+++ WorldCupPlanner/TestRepositorio/PartidoRepositorioTest.cs
@@ -2,5 +2,13 @@ namespace TestRepositorio;
 
 public class PartidoRepositorioTest
 {
+    [TestMethod]
+    public void ObtenerPartidos_CuandoNoHayPartidos_RetornaListaVacia()
+    {
+        var partidos = _partidoRepositorio.ObtenerPartidos();
+
+        Assert.IsNotNull(partidos);
+        Assert.AreEqual(0, partidos.Count);
+    }
     
 }
