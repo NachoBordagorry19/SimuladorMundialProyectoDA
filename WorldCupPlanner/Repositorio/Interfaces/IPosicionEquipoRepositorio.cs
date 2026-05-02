99cb63a5f2e53a345e7aac784a42cfb54c6c0b4e
Nacho <ignaciorubino14@gmail.com>
2026-05-01 21:16:00 -0300

    Se agregan metodos CRUD y similares en IPosicionEquipoRepositorio

--- WorldCupPlanner/Repositorio/Interfaces/IPosicionEquipoRepositorio.cs
+++ WorldCupPlanner/Repositorio/Interfaces/IPosicionEquipoRepositorio.cs
@@ -2,5 +2,13 @@ namespace Repositorio.Interfaces;
 
 public interface IPosicionEquipoRepositorio
 {
-    
+    void Agregar(PosicionEquipoDTO posicion);
+
+    void Actualizar(PosicionEquipoDTO posicion);
+
+    void Eliminar(PosicionEquipoDTO posicion);
+
+    List<PosicionEquipoDTO> ObtenerTodos();
+
+    PosicionEquipoDTO ObtenerPorNombreEquipo(string nombreEquipo);
 }
