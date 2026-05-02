c44a25f348b0ad7b99957cab6ec2ee89261d38e4
Nacho <ignaciorubino14@gmail.com>
2026-05-01 21:09:15 -0300

    Se agregan metodos CRUD y similares en IServicioPosicionEquipo

--- WorldCupPlanner/Servicios/Interfaces/IServicioPosicionEquipo.cs
+++ WorldCupPlanner/Servicios/Interfaces/IServicioPosicionEquipo.cs
@@ -2,5 +2,13 @@ namespace Servicios.Interfaces;
 
 public interface IServicioPosicionEquipo
 {
-    
+    void AgregarPosicion(PosicionEquipoDTO posicion);
+
+    void ActualizarPosicion(PosicionEquipoDTO posicion);
+
+    void EliminarPosicion(PosicionEquipoDTO posicion);
+
+    List<PosicionEquipoDTO> ObtenerPosiciones();
+
+    PosicionEquipoDTO ObtenerPosicionPorEquipo(string nombreEquipo);
 }
