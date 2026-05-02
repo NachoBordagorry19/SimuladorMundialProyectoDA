0b86140adb75c1c3744d0ff42bc2c58284a65fbe
Nacho <ignaciorubino14@gmail.com>
2026-05-01 21:40:04 -0300

    Se crea PosicionEquipoServicios en Servicios

--- WorldCupPlanner/Servicios/Clases/PosicionEquipoServicios.cs
+++ WorldCupPlanner/Servicios/Clases/PosicionEquipoServicios.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Linq;
+using Dominio.Clases;
+using Repositorio.Interfaces;
+using Servicios.Modelo;
+using Servicios.Interfaces;
+
 namespace Servicios.Clases;
 
-public class PosicionEquipoServicios
+public class PosicionEquipoServicios : IServicioPosicionEquipo
 {
     
 }
