c842a9b37f0075abf631882f8f225a790f0347a5
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-07 19:33:22 -0300

    Agrego Usuario activo en UI

---
+++ WorldCupPlanner/UI/Estado/UsuarioSesion.cs
@@ -0,0 +1,8 @@
+using Servicios.Modelo;
+
+namespace UI.Estado;
+
+public class UsuarioSesion
+{
+    public UsuarioDTO? UsuarioActual { get; set; }
+}
--- WorldCupPlanner/UI/Program.cs
+++ WorldCupPlanner/UI/Program.cs
@@ -1,9 +1,28 @@
 using UI.Components;
+using Repositorio;
+using Repositorio.Interfaces;
+using Servicios.Clases;
+using Servicios.Interfaces;
+using UI.Estado;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<BaseDeDatosEnMemoria>();
+
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+builder.Services.AddScoped<IEquipoRepositorio, EquipoRepositorio>();
+builder.Services.AddScoped<IEstadioRepositorio, EstadioRepositorio>();
+builder.Services.AddScoped<IPartidoRepositorio, PartidoRepositorio>();
+
+builder.Services.AddScoped<IServicioUsuario, ServicioUsuario>();
+builder.Services.AddScoped<EquipoServicios>();
+builder.Services.AddScoped<IServicioEstadio, EstadioServicios>();
+builder.Services.AddScoped<PartidoServicios>();
+
+builder.Services.AddScoped<UsuarioSesion>();
 
 var app = builder.Build();
 
