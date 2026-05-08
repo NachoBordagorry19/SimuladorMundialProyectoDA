5c0ed6ee27c1c93edc506cf2736cee282277cfa5
Nacho <nachobordagorry2020@gmail.com>
2026-05-08 17:11:16 -0300

    [GREEN] agrego codigo suficiente para que se genere la fixture

--- WorldCupPlanner/Servicios/Clases/FixturePrimeraFaseServicio.cs
+++ WorldCupPlanner/Servicios/Clases/FixturePrimeraFaseServicio.cs
@@ -1,6 +1,45 @@
+using Dominio.Clases;
+using Dominio.Enums;
+using Repositorio.Interfaces;
+using Servicios.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Servicios.Clases;
 
 public class FixturePrimeraFaseServicio
 {
+    private readonly IEquipoRepositorio _equipoRepositorio;
+    private readonly IEstadioRepositorio _estadioRepositorio;
+    private readonly IPartidoRepositorio _partidoRepositorio;
+    private readonly EquipoServicios _equipoServicios;
+    private readonly EstadioServicios _estadioServicios;
 
+    public FixturePrimeraFaseServicio(
+        IEquipoRepositorio equipoRepositorio,
+        IEstadioRepositorio estadioRepositorio,
+        IPartidoRepositorio partidoRepositorio,
+        EquipoServicios equipoServicios,
+        EstadioServicios estadioServicios)
+    {
+        _equipoRepositorio = equipoRepositorio;
+        _estadioRepositorio = estadioRepositorio;
+        _partidoRepositorio = partidoRepositorio;
+        _equipoServicios = equipoServicios;
+        _estadioServicios = estadioServicios;
+    }
+
+    public ResultadoFixture GenerarFixturePrimeraFase(int semillaFixture, DateTime? fechaInicio = null)
+    {
+        var resultado = new ResultadoFixture
+        {
+            SemillaFixture = semillaFixture,
+            FechaGeneracion = DateTime.UtcNow,
+            EquiposOrdenados = new List<EquipoDTO>(),
+            AuditoriaEmpates = new List<EntradaAuditoria>()
+        };
+
+        return resultado;
+    }
 }
