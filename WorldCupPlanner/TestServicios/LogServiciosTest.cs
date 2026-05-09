b15d7b315872c492759dd8727cfb9b0c5d5dfab7
Nacho <ignaciorubino14@gmail.com>
2026-05-09 18:08:28 -0300

    [RED] TestInitialize, falta crear LogRepositorio y LogServicios

--- WorldCupPlanner/TestServicios/LogServiciosTest.cs
+++ WorldCupPlanner/TestServicios/LogServiciosTest.cs
@@ -1,5 +1,6 @@
 using Dominio.Enums;
 using Repositorio;
+using Repositorio.Interfaces;
 using Servicios.Interfaces;
 using Servicios.Modelo;
 
@@ -8,5 +9,24 @@ namespace TestServicios;
 [TestClass]
 public class LogServiciosTest
 {
-    
+    private BaseDeDatosEnMemoria _baseDeDatosEnMemoria;
+    private ILogRepositorio _logRepositorio;
+    private IServicioLog _logServicios;
+    private LogDTO _logDTO;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _baseDeDatosEnMemoria = new BaseDeDatosEnMemoria();
+        _logRepositorio = new LogRepositorio(_baseDeDatosEnMemoria);
+        _logServicios = new LogServicios(_logRepositorio);
+
+        _logDTO = new LogDTO()
+        {
+            mensaje = "Inicio de sistema",
+            usuario = "admin@worldcup.com",
+            tipo = TipoLog.Info,
+            fechaISO8601 = DateTime.Now.ToString("o")
+        };
+    }
 }
