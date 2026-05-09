445415e6897b1e49fd944103a677872d8f69aefa
Nacho <ignaciorubino14@gmail.com>
2026-05-09 20:17:19 -0300

    se implementa lo minimo para que no de error en LogServicios

--- WorldCupPlanner/Servicios/Clases/LogServicios.cs
+++ WorldCupPlanner/Servicios/Clases/LogServicios.cs
@@ -1,6 +1,24 @@
+using Repositorio.Interfaces;
+using Servicios.Interfaces;
+using Servicios.Modelo;
+
 namespace Servicios.Clases;
 
 public class LogServicios : IServicioLog
 {
-    
+    private readonly ILogRepositorio _logRepo;
+
+    public LogServicios(ILogRepositorio logRepo)
+    {
+        _logRepo = logRepo;
+    }
+
+    public void GuardarLog(LogDTO dto)
+    {
+    }
+
+    public List<LogDTO> ObtenerLogs()
+    {
+        return new List<LogDTO>();
+    }
 }
