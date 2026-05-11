3fc67ed27919df51e37216832945fd3d19cca6fa
Nacho <ignaciorubino14@gmail.com>
2026-05-11 15:08:40 -0300

    Se eliminan firmas previas de IServicioLog para nueva implementacion de Log

--- WorldCupPlanner/Servicios/Interfaces/IServicioLog.cs
+++ WorldCupPlanner/Servicios/Interfaces/IServicioLog.cs
@@ -5,9 +5,6 @@ namespace Servicios.Interfaces;
 
 public interface IServicioLog
 {
-    void GuardarLog(LogDTO dto);
-    List<LogDTO> ObtenerLogs();
-    List<LogDTO> ObtenerLogsPorTipo(TipoLog tipo);
     void RegistrarAltaEquipo(string nombre, string confederacion);
     void RegistrarEdicionEquipo(string nombre);
     void RegistrarEliminacionEquipo(string nombre);
