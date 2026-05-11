5137c4942d9b5899532503b2c6dede7aa9e2bd74
Nacho <ignaciorubino14@gmail.com>
2026-05-11 15:11:35 -0300

    Se actualizan implementaciones en LogServicios minimas para que no den error

--- WorldCupPlanner/Servicios/Clases/LogServicios.cs
+++ WorldCupPlanner/Servicios/Clases/LogServicios.cs
@@ -14,62 +14,19 @@ public class LogServicios : IServicioLog
     {
         _logRepo = logRepo;
     }
-
-    public void GuardarLog(LogDTO dto)
+    
+    public void RegistrarAltaEquipo(string nombre, string confederacion)
     {
-        Log nuevoLog = new Log();
-        nuevoLog.Mensaje = dto.mensaje;
-        nuevoLog.Usuario = dto.usuario;
-        nuevoLog.Tipo = dto.tipo;
-        DateTime fechaParseada;
-        bool esFechaValida = DateTime.TryParse(dto.fechaISO8601, out fechaParseada);
-
-        if (esFechaValida)
-        {
-            nuevoLog.Fecha = fechaParseada;
-        }
-        else
-        {
-            nuevoLog.Fecha = DateTime.Now;
-        }
-        nuevoLog.Validar();
-        _logRepo.AgregarLog(nuevoLog);
+        throw new NotImplementedException();
     }
 
-    public List<LogDTO> ObtenerLogs()
+    public void RegistrarEdicionEquipo(string nombre)
     {
-        List<Log> entidades = _logRepo.ObtenerLogs();
-        List<LogDTO> listaDtos = new List<LogDTO>();
-
-        foreach (Log log in entidades)
-        {
-            LogDTO dto = new LogDTO();
-            dto.mensaje = log.Mensaje;
-            dto.usuario = log.Usuario;
-            dto.tipo = log.Tipo;
-            dto.fechaISO8601 = log.Fecha.ToString("o");
-        
-            listaDtos.Add(dto);
-        }
-
-        return listaDtos;
+        throw new NotImplementedException();
     }
-    
-    public List<LogDTO> ObtenerLogsPorTipo(TipoLog tipo)
+
+    public void RegistrarEliminacionEquipo(string nombre)
     {
-        List<Log> logs = _logRepo.ObtenerLogsPorTipo(tipo);
-    
-        List<LogDTO> logsPorTipo = new List<LogDTO>();
-        foreach (var log in logs)
-        {
-            logsPorTipo.Add(new LogDTO 
-            { 
-                mensaje = log.Mensaje, 
-                usuario = log.Usuario, 
-                tipo = log.Tipo, 
-                fechaISO8601 = log.Fecha.ToString("o") 
-            });
-        }
-        return logsPorTipo;
+        throw new NotImplementedException();
     }
 }
