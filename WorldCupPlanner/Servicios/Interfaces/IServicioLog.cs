2c98eea6dc89c367a799859b542df51d75e63e57
Nacho <ignaciorubino14@gmail.com>
2026-05-11 15:04:29 -0300

    Se modifica IServicioLog para nueva implementacion de Log

--- WorldCupPlanner/Servicios/Interfaces/IServicioLog.cs
+++ WorldCupPlanner/Servicios/Interfaces/IServicioLog.cs
@@ -8,4 +8,7 @@ public interface IServicioLog
     void GuardarLog(LogDTO dto);
     List<LogDTO> ObtenerLogs();
     List<LogDTO> ObtenerLogsPorTipo(TipoLog tipo);
+    void RegistrarAltaEquipo(string nombre, string confederacion);
+    void RegistrarEdicionEquipo(string nombre);
+    void RegistrarEliminacionEquipo(string nombre);
 }
