018524bb309edcf192e6f24d5ddc595897753638
Nacho <ignaciorubino14@gmail.com>
2026-05-12 18:47:38 -0300

    Se elimina Log de BaseDeDatosEnMemoria

--- WorldCupPlanner/Repositorio/BaseDeDatosEnMemoria.cs
+++ WorldCupPlanner/Repositorio/BaseDeDatosEnMemoria.cs
@@ -9,7 +9,6 @@ public class BaseDeDatosEnMemoria
     private List<Equipo> _listaDeEquipos { get; }
     private List<Estadio> _listaDeEstadios { get; }
     private List<Partido> _listaDePartidos { get; }
-    private List<Log> _listaDeLogs { get; }
 
     public BaseDeDatosEnMemoria()
     {
@@ -17,7 +16,6 @@ public class BaseDeDatosEnMemoria
         _listaDeEquipos = new List<Equipo>();
         _listaDeEstadios = new List<Estadio>();
         _listaDePartidos = new List<Partido>();
-        _listaDeLogs = new List<Log>();
     }
 
     public void AgregarUsuario(Usuario usuario)
@@ -109,21 +107,5 @@ public class BaseDeDatosEnMemoria
         Partido? partidoParaActualizar = _listaDePartidos.Find(p => p.Id == partido.Id);
         var indicePartido = _listaDePartidos.IndexOf(partidoParaActualizar);
         _listaDePartidos[indicePartido] = partido;
-    }
-
-    public List<Log> ObtenerLogs()
-    {
-        List<Log> copia = new List<Log>();
-        foreach (Log log in _listaDeLogs)
-        {
-            copia.Add(log);
-        }
-        return copia;
-    }
-
-    public void AgregarLog(Log log)
-    {
-        log.Id = _listaDeLogs.Count + 1;
-        _listaDeLogs.Add(log);
     }
 }
