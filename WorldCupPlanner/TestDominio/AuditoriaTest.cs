a73f260d33df84101c5e3d1e808345c0c469e916
Nacho <ignaciorubino14@gmail.com>
2026-05-12 19:01:40 -0300

    [RED] CrearAuditoria_AsignaPropiedadesCorrectamente()

--- WorldCupPlanner/TestDominio/AuditoriaTest.cs
+++ WorldCupPlanner/TestDominio/AuditoriaTest.cs
@@ -1,6 +1,24 @@
 namespace TestDominio;
 
+[TestClass]
 public class AuditoriaTest
 {
-    
+    [TestMethod]
+    public void CrearAuditoria_AsignaPropiedadesCorrectamente()
+    {
+        DateTime ahora = DateTime.Now;
+        
+        var auditoria = new Auditoria
+        {
+            Usuario = "admin@cup.com",
+            Accion = "Login",
+            Detalle = "Se logueó correctamente",
+            FechaHora = ahora
+        };
+
+        Assert.AreEqual("admin@cup.com", auditoria.Usuario);
+        Assert.AreEqual("Login", auditoria.Accion);
+        Assert.AreEqual("Se logueó correctamente", auditoria.Detalle);
+        Assert.AreEqual(ahora, auditoria.FechaHora);
+    }
 }
