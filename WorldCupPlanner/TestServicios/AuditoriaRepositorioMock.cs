b32bc79886df735ed027f91a14ec2a850c9fac96
Nacho <ignaciorubino14@gmail.com>
2026-05-12 19:20:35 -0300

    Se implementa AuditoriaRepositorioMock

---
+++ WorldCupPlanner/TestServicios/AuditoriaRepositorioMock.cs
@@ -0,0 +1,11 @@
+using Dominio.Clases;
+using Repositorio.Interfaces;
+
+namespace TestServicios;
+
+public class AuditoriaRepositorioMock : IAuditoriaRepositorio
+{
+    private readonly List<Auditoria> _datos = new();
+    public void AgregarRegistro(Auditoria registro) => _datos.Add(registro);
+    public List<Auditoria> ObtenerTodosLosRegistros() => _datos;
+}
