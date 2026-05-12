60588c976f1aec9bdcddfd973bbecc97d1fe59e2
Nacho <ignaciorubino14@gmail.com>
2026-05-12 19:41:53 -0300

    [RED] RegistrarAltaUsuario_DebeGuardarRegistroCorrectamente()

--- WorldCupPlanner/TestServicios/AuditoriaServicioTest.cs
+++ WorldCupPlanner/TestServicios/AuditoriaServicioTest.cs
@@ -16,4 +16,21 @@ public class AuditoriaServicioTest
         _sessionMock = new SessionServiceMock();
         _servicio = new ServicioAuditoria(_sessionMock, _repoMock);
     }
+    
+    [TestMethod]
+    public void RegistrarAltaUsuario_DebeGuardarRegistroCorrectamente()
+    {
+        string email = "usuario@gmail.com";
+        string rol = "Administrador";
+        _servicio.RegistrarAltaUsuario(email, rol);
+        var registros = _servicio.ObtenerRegistrosFormateados();
+            
+        Assert.AreEqual(1, registros.Count);
+        string log = registros[0];
+        Assert.IsTrue(log.Contains("Alta Usuario"));
+        Assert.IsTrue(log.Contains(email));
+        Assert.IsTrue(log.Contains("admin@gmail.com"));
+        string fechaHoy = DateTime.Now.ToString("yyyy-MM-dd");
+        Assert.IsTrue(log.StartsWith(fechaHoy));
+    }
 }
