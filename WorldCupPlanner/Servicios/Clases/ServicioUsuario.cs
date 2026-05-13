8945599a2811a1c4442e7b66d09a8c1c7076bb21
Nacho <ignaciorubino14@gmail.com>
2026-05-13 02:04:17 -0300

    [GREEN] Se modifica AgregarUsuario() para que registre en auditoria

--- WorldCupPlanner/Servicios/Clases/ServicioUsuario.cs
+++ WorldCupPlanner/Servicios/Clases/ServicioUsuario.cs
@@ -31,6 +31,9 @@ public class ServicioUsuario : IServicioUsuario
         ValidarEmailExiste(emailAVerificar);
         ValidarRoles(usuarioDto);
         _usuarioRepositorio.AgregarUsuario(usuario);
+        
+        string rolesString = string.Join(", ", usuarioDto.Roles.Select(r => r.ToString()));
+        _auditoria.RegistrarAltaUsuario(usuarioDto.Email, rolesString);
     }
 
     public void ValidarRoles(UsuarioDTO usuarioDto)
