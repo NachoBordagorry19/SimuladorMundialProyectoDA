be63c6cd927994a8637d857bacdb5b1e99e0af71
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-13 15:00:28 -0300

    [RED] Test admin edita estadio

--- WorldCupPlanner/TestServicios/ServicioAutorizacionTest.cs
+++ WorldCupPlanner/TestServicios/ServicioAutorizacionTest.cs
@@ -43,4 +43,18 @@ public class ServicioAutorizacionTest
 
         Assert.IsTrue(resultado);
     }
+    
+    [TestMethod]
+    public void Administrador_PuedeGestionarEstadios()
+    {
+        UsuarioDTO usuario = new UsuarioDTO
+        {
+            Email = "admin@gmail.com",
+            Roles = new List<Rol> { Rol.Administrador }
+        };
+
+        bool resultado = _servicioAutorizacion.PuedeGestionarEstadios(usuario);
+
+        Assert.IsTrue(resultado);
+    }
 }
