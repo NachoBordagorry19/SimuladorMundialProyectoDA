c29b6bb5207ca2114a14eff6b551f048f1abeb20
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-13 15:14:25 -0300

    [RED] Agrego test admin auditoria

--- WorldCupPlanner/TestServicios/ServicioAutorizacionTest.cs
+++ WorldCupPlanner/TestServicios/ServicioAutorizacionTest.cs
@@ -57,4 +57,18 @@ public class ServicioAutorizacionTest
 
         Assert.IsTrue(resultado);
     }
+    
+    [TestMethod]
+    public void Administrador_PuedeVisualizarLogs()
+    {
+        UsuarioDTO usuario = new UsuarioDTO
+        {
+            Email = "admin@gmail.com",
+            Roles = new List<Rol> { Rol.Administrador }
+        };
+
+        bool resultado = _servicioAutorizacion.PuedeVisualizarLogs(usuario);
+
+        Assert.IsTrue(resultado);
+    }
 }
