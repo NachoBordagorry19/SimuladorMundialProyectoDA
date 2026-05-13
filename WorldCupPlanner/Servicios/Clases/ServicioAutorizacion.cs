76e6898bb9c76bcf02d7333c036c8de89342ecb6
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-13 14:39:14 -0300

    [GREEN] Implemento admin- gestion de usuarios

--- WorldCupPlanner/Servicios/Clases/ServicioAutorizacion.cs
+++ WorldCupPlanner/Servicios/Clases/ServicioAutorizacion.cs
@@ -1,6 +1,48 @@
+using Dominio.Enums;
+using Servicios.Interfaces;
+using Servicios.Modelo;
+
 namespace Servicios.Clases;
 
-public class ServicioAutorizacion
+public class ServicioAutorizacion : IServicioAutorizacion
 {
-    
+    public bool PuedeGestionarUsuarios(UsuarioDTO usuario)
+    {
+        return usuario.Roles.Contains(Rol.Administrador);
+    }
+
+    public bool PuedeGestionarEquipos(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeGestionarEstadios(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeVisualizarLogs(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeUsarFixture(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeEditarPartidos(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeImportar(UsuarioDTO? usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool PuedeEliminarUsuario(UsuarioDTO? usuarioActual, string emailAEliminar)
+    {
+        throw new NotImplementedException();
+    }
 }
