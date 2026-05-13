0ee951dda9156d05835227b537f9a01f38676028
Fabrizio Perotti <fperotti2323@gmail.com>
2026-05-13 15:16:46 -0300

    [GREEN] Implemento uso de fixture para editor

--- WorldCupPlanner/Servicios/Clases/ServicioAutorizacion.cs
+++ WorldCupPlanner/Servicios/Clases/ServicioAutorizacion.cs
@@ -26,9 +26,9 @@ public class ServicioAutorizacion : IServicioAutorizacion
         return usuario.Roles.Contains(Rol.Administrador);
     }
 
-    public bool PuedeUsarFixture(UsuarioDTO? usuario)
+    public bool PuedeUsarFixture(UsuarioDTO usuario)
     {
-        throw new NotImplementedException();
+        return usuario.Roles.Contains(Rol.Editor);
     }
 
     public bool PuedeEditarPartidos(UsuarioDTO? usuario)
