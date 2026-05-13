63c82aaa3b46351958c3f88d86de098244fbadf6
Nacho <ignaciorubino14@gmail.com>
2026-05-12 22:39:34 -0300

    Se declara RegistrarAltaEquipo() en IServicioAuditoria y refactor (se ordenan las declaraciones)

--- WorldCupPlanner/Servicios/Interfaces/IServicioAuditoria.cs
+++ WorldCupPlanner/Servicios/Interfaces/IServicioAuditoria.cs
@@ -3,17 +3,18 @@ namespace Servicios.Interfaces;
 public interface IServicioAuditoria
 {
     void RegistrarAltaUsuario(string email, string roles);
-    List<string> ObtenerRegistrosFormateados();
-    void RegistrarEdicionEquipo(string nombre);
-    void RegistrarEliminacionEquipo(string nombre);
     void RegistrarEdicionUsuario(string email);
     void RegistrarEliminacionUsuario(string email);
     void RegistrarAltaEstadio(string nombre);
     void RegistrarEdicionEstadio(string nombre);
     void RegistrarEliminacionEstadio(string nombre);
-    void RegistrarModificacionPartido(string detalle);
-    void RegistrarImportacionEquipos(string mensaje, bool esExito);
+    void RegistrarAltaEquipo(string nombre);
+    void RegistrarEdicionEquipo(string nombre);
+    void RegistrarEliminacionEquipo(string nombre);
     void RegistrarGeneracionAutomaticaEquipos(int cantidad);
     void RegistrarGeneracionFixture();
+    void RegistrarModificacionPartido(string detalle);
     void RegistrarSorteoCruces();
+    void RegistrarImportacionEquipos(string mensaje, bool esExito);
+    List<string> ObtenerRegistrosFormateados();
 }
