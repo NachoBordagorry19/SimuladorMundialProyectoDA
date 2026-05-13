3ab267eb780f303daca625d56d14cb72d0a9fecd
Nacho <ignaciorubino14@gmail.com>
2026-05-12 21:01:15 -0300

    [RED] RegistrarEliminacionEquipo_DebeGuardarRegistroCorrectamente()

--- WorldCupPlanner/TestServicios/AuditoriaServicioTest.cs
+++ WorldCupPlanner/TestServicios/AuditoriaServicioTest.cs
@@ -67,4 +67,20 @@ public class AuditoriaServicioTest
         string fechaHoy = DateTime.Now.ToString("yyyy-MM-dd");
         Assert.IsTrue(log.StartsWith(fechaHoy));
     }
+    
+    [TestMethod]
+    public void RegistrarEliminacionEquipo_DebeGuardarRegistroCorrectamente()
+    {
+        string nombre = "Italia";
+        _servicio.RegistrarEliminacionEquipo(nombre);
+        var registros = _servicio.ObtenerRegistrosFormateados();
+
+        Assert.AreEqual(1, registros.Count);
+        string log = registros[0];
+        Assert.IsTrue(log.Contains("Eliminación Equipo"));
+        Assert.IsTrue(log.Contains(nombre));
+        Assert.IsTrue(log.Contains("admin@gmail.com"));
+        string fechaHoy = DateTime.Now.ToString("yyyy-MM-dd");
+        Assert.IsTrue(log.StartsWith(fechaHoy));
+    }
 }
