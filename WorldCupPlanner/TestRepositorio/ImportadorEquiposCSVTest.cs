34470eb6411d532eec85c655d37ffbce3400f99b
Nacho <nachobordagorry2020@gmail.com>
2026-05-13 09:43:19 -0300

    [RED] realizo prueba para validar que no venga con nombre vacio el archivo CSV

--- WorldCupPlanner/TestRepositorio/ImportadorEquiposCSVTest.cs
+++ WorldCupPlanner/TestRepositorio/ImportadorEquiposCSVTest.cs
@@ -167,6 +167,26 @@ public class ImportadorEquiposCSVTest
         }
     }
 
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ImportarCSV_ConNombreVacio_LanzaExcepcion()
+    {
+        string rutaArchivoCSV = CrearArchivoCSVTemporal(
+            "Nombre,Confederación,RankingFIFA\n" +
+            ",CONMEBOL,1500"
+        );
+
+
+        try
+        {
+            _importadorEquipos.ImportarDesdeCSV(rutaArchivoCSV);
+        }
+        finally
+        {
+            LimpiarArchivoCSV(rutaArchivoCSV);
+        }
+    }
+
 
     
     private string CrearArchivoCSVTemporal(string contenido)
