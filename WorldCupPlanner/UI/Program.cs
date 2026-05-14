ab583701e53f42fd14b585fd88f8d362841a5047
Nacho <nachobordagorry2020@gmail.com>
2026-05-14 17:12:24 -0300

    Cambio para usuario por defecto

--- WorldCupPlanner/UI/Program.cs
+++ WorldCupPlanner/UI/Program.cs
@@ -38,23 +38,6 @@ if (app.Environment.IsDevelopment())
 {
     using IServiceScope scope = app.Services.CreateScope();
     IServicioUsuario servicioUsuario = scope.ServiceProvider.GetRequiredService<IServicioUsuario>();
-
-    bool existeAdmin = servicioUsuario
-        .ObtenerUsuarios()
-        .Any(usuario => usuario.Email == "admin@admin.com");
-
-    if (!existeAdmin)
-    {
-        servicioUsuario.AgregarUsuario(new UsuarioDTO()
-        {
-            Nombre = "Admin",
-            Apellido = "Prueba",
-            Email = "admin@admin.com",
-            FechaNacimiento = new DateTime(2000, 1, 1),
-            Contraseña = "Admin123!",
-            Roles = new List<Rol> { Rol.Administrador, Rol.Editor }
-        });
-    }
 }
 
 if (!app.Environment.IsDevelopment())
