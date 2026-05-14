77118051781e2d3f9a723c6775eeb5359385855c
Nacho <nachobordagorry2020@gmail.com>
2026-05-14 15:54:04 -0300

    [REFACTOR] aplico clean code al codigo en dominio

--- WorldCupPlanner/Dominio/Clases/Usuario.cs
+++ WorldCupPlanner/Dominio/Clases/Usuario.cs
@@ -92,7 +92,7 @@ public class Usuario
             }
             else if (!value.Any(char.IsLower))
             {
-                throw new ArgumentException("La contraseña debe contener una letra minusucla");
+                throw new ArgumentException("La contraseña debe contener una letra minuscula");
             }
             else if (!value.Any(char.IsDigit))
                 throw new ArgumentException("Contraseña debe contener al menos un número");
