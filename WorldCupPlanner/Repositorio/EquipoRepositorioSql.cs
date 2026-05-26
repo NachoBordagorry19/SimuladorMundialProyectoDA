f702a234a047d84999495750541b277faa04e3ec
Nacho <nachobordagorry2020@gmail.com>
2026-05-26 18:03:24 -0300

    [GREEN] realizo codigo para que la prueba de actualizar equipo funcione

--- WorldCupPlanner/Repositorio/EquipoRepositorioSql.cs
+++ WorldCupPlanner/Repositorio/EquipoRepositorioSql.cs
@@ -33,4 +33,10 @@ public class EquipoRepositorioSql
     {
         return _contexto.Equipos.ToList().Where(filtro).FirstOrDefault();
     }
+
+    public void ActualizarEquipo(Equipo equipo)
+    {
+        _contexto.Equipos.Update(equipo);
+        _contexto.SaveChanges();
+    }
 }
