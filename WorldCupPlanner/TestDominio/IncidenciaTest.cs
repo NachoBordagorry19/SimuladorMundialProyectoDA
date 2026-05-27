81f0f71031c78b06b42295e5df2c8bedc141a3a0
Nacho <nachobordagorry2020@gmail.com>
2026-05-27 16:32:30 -0300

    [REFACTOR] agrego test ya verificado

--- WorldCupPlanner/TestDominio/IncidenciaTest.cs
+++ WorldCupPlanner/TestDominio/IncidenciaTest.cs
@@ -18,4 +18,20 @@ public class IncidenciaTest
         Assert.AreEqual(equipoId, incidencia._idEquipo);
         Assert.AreEqual(tipo, incidencia._tipoIncidencia);
     }
+    
+    [TestMethod]
+    public void CrearIncidencia_ConTarjetaRoja_DebeCrearseCorrectamente()
+    {
+        int partidoId = 2;
+        int equipoId = 2;
+        var tipo = TipoIncidencia.TarjetaRoja;
+
+
+        var incidencia = new Incidencia(partidoId, equipoId, tipo);
+
+
+        Assert.AreEqual(partidoId, incidencia._idPartido);
+        Assert.AreEqual(equipoId, incidencia._idEquipo);
+        Assert.AreEqual(tipo, incidencia._tipoIncidencia);
+    }
 }
