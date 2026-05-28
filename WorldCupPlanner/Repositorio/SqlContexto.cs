f69d6e1a228c56ad15b98a3c682a2d8c1effabf7
Nacho <ignaciorubino14@gmail.com>
2026-05-28 20:54:44 -0300

    DbSet Auditorias en SqlContexto

--- WorldCupPlanner/Repositorio/SqlContexto.cs
+++ WorldCupPlanner/Repositorio/SqlContexto.cs
@@ -8,9 +8,9 @@ public class SqlContexto: DbContext
 {
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<Equipo> Equipos { get; set; }
-    
     public DbSet<Estadio> Estadios { get; set; }
     public DbSet<Partido> Partidos { get; set; }
+    public DbSet<Auditoria> Auditorias { get; set; }
 
     public SqlContexto(DbContextOptions<SqlContexto> options) : base(options)
     {
