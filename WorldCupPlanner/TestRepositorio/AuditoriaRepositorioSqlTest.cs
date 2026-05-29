db06c43a04b1fb1f3be7f39c210112c9cecf3c9f
Nacho <ignaciorubino14@gmail.com>
2026-05-29 03:44:52 -0300

    [RED] AgregarRegistro_SiRegistroValido_LoGuarda()

--- WorldCupPlanner/TestRepositorio/AuditoriaRepositorioSqlTest.cs
+++ WorldCupPlanner/TestRepositorio/AuditoriaRepositorioSqlTest.cs
@@ -24,4 +24,23 @@ public class AuditoriaRepositorioSqlTest
         var registros = _auditoriaRepositorioSql.ObtenerTodosLosRegistros();
         Assert.AreEqual(0, registros.Count);
     }
+    
+    [TestMethod]
+    public void AgregarRegistro_SiRegistroValido_LoGuarda()
+    {
+        var registro = new Auditoria
+        {
+            Usuario = "admin@gmail.com",
+            Accion = "Alta usuario",
+            Detalle = "Se agregó un usuario"
+        };
+
+        _auditoriaRepositorioSql.AgregarRegistro(registro);
+
+        var registros = _auditoriaRepositorioSql.ObtenerTodosLosRegistros();
+
+        Assert.AreEqual(1, registros.Count);
+        Assert.AreEqual("admin@gmail.com", registros[0].Usuario);
+        Assert.AreEqual("Alta usuario", registros[0].Accion);
+    }
 }
