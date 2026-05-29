0cefba9392dd21135a986ffeb57d1916dbcc0da6
Nacho <nachobordagorry2020@gmail.com>
2026-05-29 02:41:51 -0300

    Agrego dto de incidencia

--- WorldCupPlanner/Servicios/Modelo/IncidenciaDTO.cs
+++ WorldCupPlanner/Servicios/Modelo/IncidenciaDTO.cs
@@ -1,1 +1,11 @@
+namespace Servicios.Modelo;
 
+using Dominio.Enums;
+
+public class IncidenciaDTO
+{
+    public int Id { get; set; }
+    public int IdPartido { get; set; }
+    public int IdEquipo { get; set; }
+    public TipoIncidencia TipoIncidencia { get; set; }
+}
