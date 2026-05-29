f21ccc25fd63711a2d38def8604bff2bb85657f0
Nacho <nachobordagorry2020@gmail.com>
2026-05-29 02:52:26 -0300

    [RED] realizo codigo para agregar incidencia

---
+++ WorldCupPlanner/TestServicios/IncidenciaServicioTest.cs
@@ -0,0 +1,42 @@
+using Dominio.Clases;
+using Dominio.Enums;
+using Repositorio;
+using Repositorio.Interfaces;
+using Servicios.Modelo;
+
+namespace TestServicios;
+
+[TestClass]
+public class IncidenciaServicioTest
+{
+    private SqlContexto _contexto;
+    private FabricaDeContextoDeAppEnMemoria _contextoFabrica;
+    private IIncidenciaRepositorio _incidenciaRepositorio;
+    private IncidenciaServicio  _incidenciaServicio;
+    private IncidenciaDTO  _incidenciaDTO;
+    private Incidencia _incidencia;
+
+    [TestInitialize]
+    public void IniciarPrueba()
+    {
+        _contextoFabrica = new FabricaDeContextoDeAppEnMemoria();
+        _contexto = _contextoFabrica.CrearDbContexto();
+        _contexto.Incidencias.RemoveRange(_contexto.Incidencias);
+        _contexto.SaveChanges();
+        _incidencia = new Incidencia();
+        _incidenciaDTO = new  IncidenciaDTO()
+        {
+            Id = _incidencia.Id,
+            IdEquipo = 2,
+            IdPartido = 2,
+            TipoIncidencia = TipoIncidencia.TarjetaAmarilla
+        };
+    }
+
+    [TestMethod]
+    public void AgregarIncidenciSiNoExiste_SeAgregarCorrectamente()
+    {
+        _incidenciaServicio.AgregarIncidencia(_incidenciaDTO);
+        Assert.AreEqual(2,_incidenciaDTO.IdEquipo);
+    }
+}
