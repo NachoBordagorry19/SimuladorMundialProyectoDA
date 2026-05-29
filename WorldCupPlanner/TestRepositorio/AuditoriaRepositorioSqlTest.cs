ff52fd761ef2bb1ed4cf92ae275a4c3b13c31baa
Nacho <ignaciorubino14@gmail.com>
2026-05-29 02:57:16 -0300

    [RED] Se inicializa test - error: falta agregar constructor a AuditoriaRepositorioSql

--- WorldCupPlanner/TestRepositorio/AuditoriaRepositorioSqlTest.cs
+++ WorldCupPlanner/TestRepositorio/AuditoriaRepositorioSqlTest.cs
@@ -1,6 +1,20 @@
-namespace TestRepositorio;
+using Dominio.Clases;
+using Repositorio;
 
+namespace TestRepositorio;
+[TestClass]
 public class AuditoriaRepositorioSqlTest
 {
-    
+    private SqlContexto _contexto;
+    private FabricaDeContextoDeAppEnMemoria _contextoFabrica;
+    private AuditoriaRepositorioSql _auditoriaRepositorioSql;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _contextoFabrica = new FabricaDeContextoDeAppEnMemoria();
+        _contexto = _contextoFabrica.CrearDbContexto();
+        _auditoriaRepositorioSql = new AuditoriaRepositorioSql(_contexto);
+        _contexto.SaveChanges();
+    }
 }
