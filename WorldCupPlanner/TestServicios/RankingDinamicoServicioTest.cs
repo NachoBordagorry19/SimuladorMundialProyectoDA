686e8932d3be0961819dd04a05628835915f2882
Nacho <nachobordagorry2020@gmail.com>
2026-06-07 20:57:55 -0300

    [RED] agrego prueba para que si local gano el partido ranking local aumente y ranking visitante baje

--- WorldCupPlanner/TestServicios/RankingDinamicoServicioTest.cs
+++ WorldCupPlanner/TestServicios/RankingDinamicoServicioTest.cs
@@ -70,7 +70,17 @@ public class RankingDinamicoServicioTest
         var visitanteActualizado = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == "Bayern");
         Assert.AreEqual(301, localActualizado.RankingFifa);
         Assert.AreEqual(302, visitanteActualizado.RankingFifa);
-
     }
 
+    [TestMethod]
+    public void ActualizarRanking_LocalGanaContraEquipoIgual_RankingLocalAumentaYVisitanteDisminuye()
+    {
+        AgregarEquipos("Barcelona",1500,"Bayern",1500);
+        PartidoDTO partidoDto = CrearPartidoDTO("Barcelona","Bayern",2,0,Fase.Grupos);
+        _servicioRankingDinamico.ActualizarRanking(partidoDto);
+        var localActualizado = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == "EquipoA");
+        var visitanteActualizado = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == "EquipoB");
+        Assert.AreEqual(1515, localActualizado.RankingFifa);
+        Assert.AreEqual(1485, visitanteActualizado.RankingFifa);
+    }
 }
