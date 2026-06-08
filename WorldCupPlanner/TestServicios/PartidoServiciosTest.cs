903629d88c9b54d024fb665759d974bb26f39d3e
Fabrizio Perotti <fperotti2323@gmail.com>
2026-06-08 14:05:39 -0300

    [REFACTOR] Agrego tests para tarjetas rojas

--- WorldCupPlanner/TestServicios/PartidoServiciosTest.cs
+++ WorldCupPlanner/TestServicios/PartidoServiciosTest.cs
@@ -354,4 +354,18 @@ public class PartidoServiciosTest
         Assert.IsTrue(partidoSimulado.incidenciaEquipoLocal.Count >= 0 && partidoSimulado.incidenciaEquipoLocal.Count <= 10);
         Assert.IsTrue(partidoSimulado.incidenciaEquipoVisitante.Count >= 0 && partidoSimulado.incidenciaEquipoVisitante.Count <= 10);
     }
+
+    [TestMethod]
+    public void SimularResultado_GeneraTarjetasRojasEnRangoValido()
+    {
+        _servicioPartido.AgregarPartido(_partidoDTO, equipoLocalDTO, equipoVisitanteDTO, estadioDTO);
+
+        _servicioPartido.SimularResultado(_partidoDTO, 12345);
+
+        PartidoDTO partidoSimulado = _servicioPartido.ObtenerPartido(_partidoDTO.idPartido);
+        int rojasLocal = partidoSimulado.incidenciaEquipoLocal.Count(i => i == TipoIncidencia.TarjetaRoja);
+        int rojasVisitante = partidoSimulado.incidenciaEquipoVisitante.Count(i => i == TipoIncidencia.TarjetaRoja);
+        Assert.IsTrue(rojasLocal >= 0 && rojasLocal <= 5);
+        Assert.IsTrue(rojasVisitante >= 0 && rojasVisitante <= 5);
+    }
 }
