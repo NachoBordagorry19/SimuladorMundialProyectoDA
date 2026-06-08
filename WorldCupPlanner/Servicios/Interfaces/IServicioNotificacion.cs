99e7304127a67dd4182bbbe0e2670bf3e93b13fa
Nacho <ignaciorubino14@gmail.com>
2026-06-08 20:04:48 -0300

    Se crea IServicioNotificacion

---
+++ WorldCupPlanner/Servicios/Interfaces/IServicioNotificacion.cs
@@ -0,0 +1,10 @@
+using Dominio.Clases;
+
+namespace Servicios.Interfaces;
+
+public interface IServicioNotificacion
+{
+    void GenerarNotificacionesParaPeriodistas(string mensaje);
+    List<Notificacion> ObtenerNoLeidas(int usuarioId);
+    void MarcarComoLeida(int notificacionId);
+}
