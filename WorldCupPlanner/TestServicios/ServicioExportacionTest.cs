499e85f906ca7275b9503e02300e38334ed34d7f
Fabrizio Perotti <fperotti2323@gmail.com>
2026-06-08 18:39:31 -0300

    [REFACTOR] Agrego tests en exportar para bytes no vacios

---
+++ WorldCupPlanner/TestServicios/ServicioExportacionTest.cs
@@ -0,0 +1,26 @@
+using Dominio.Clases;
+using Servicios.Clases;
+
+namespace TestServicios;
+
+[TestClass]
+public class ServicioExportacionTest
+{
+    private AuditoriaRepositorioMock _repoMock;
+    private ServicioExportacion _servicio;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repoMock = new AuditoriaRepositorioMock();
+        _servicio = new ServicioExportacion(_repoMock);
+    }
+
+    [TestMethod]
+    public void ExportarAuditoriaCsv_ConRegistros_DevuelveBytesNoVacios()
+    {
+        _repoMock.AgregarRegistro(new Auditoria { Usuario = "admin@test.com", Accion = "Alta Usuario", Detalle = "Test" });
+        byte[] resultado = _servicio.ExportarAuditoriaCsv(DateTime.MinValue, DateTime.MaxValue);
+        Assert.IsTrue(resultado.Length > 0);
+    }
+}
