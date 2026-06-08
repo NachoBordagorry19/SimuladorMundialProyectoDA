28432038c5714877c874cbee5f0299089003b4e4
Fabrizio Perotti <fperotti2323@gmail.com>
2026-06-08 13:20:16 -0300

    [RED] Tests para incidencias en simulacion

--- WorldCupPlanner/TestServicios/PartidoServiciosTest.cs
+++ WorldCupPlanner/TestServicios/PartidoServiciosTest.cs
@@ -342,4 +342,16 @@ public class PartidoServiciosTest
         _partidoDTO.incidenciaEquipoVisitante.Add(TipoIncidencia.TarjetaAmarilla);
         _servicioPartido.AgregarPartido(_partidoDTO, equipoLocalDTO, equipoVisitanteDTO, estadioDTO);
     }
+
+    [TestMethod]
+    public void SimularResultado_GeneraIncidenciasParaAmbosEquipos()
+    {
+        _servicioPartido.AgregarPartido(_partidoDTO, equipoLocalDTO, equipoVisitanteDTO, estadioDTO);
+
+        _servicioPartido.SimularResultado(_partidoDTO, 12345);
+
+        PartidoDTO partidoSimulado = _servicioPartido.ObtenerPartido(_partidoDTO.idPartido);
+        Assert.IsTrue(partidoSimulado.incidenciaEquipoLocal.Count > 0);
+        Assert.IsTrue(partidoSimulado.incidenciaEquipoVisitante.Count > 0);
+    }
 }
