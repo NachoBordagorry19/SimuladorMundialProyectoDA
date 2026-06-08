803eb95f8598e5c36435f8782dd044334331e8e5
Nacho <nachobordagorry2020@gmail.com>
2026-06-07 21:09:22 -0300

    [RED] agrego prueba para que si es fase eliminatoria usa multiplicador de 1 punto 5

--- WorldCupPlanner/TestServicios/RankingDinamicoServicioTest.cs
+++ WorldCupPlanner/TestServicios/RankingDinamicoServicioTest.cs
@@ -83,4 +83,19 @@ public class RankingDinamicoServicioTest
         Assert.AreEqual(1515, localActualizado.RankingFifa);
         Assert.AreEqual(1485, visitanteActualizado.RankingFifa);
     }
+    
+    [TestMethod]
+    public void ActualizarRanking_FaseEliminatoria_UsaMultiplicador1punto5()
+    {
+        AgregarEquipos("Barcelona", 1500, "Bayern", 1500);
+        PartidoDTO partidoDto = CrearPartidoDTO("Barcelona", "Bayern", 1, 0, Fase.Cuartos);
+
+        _servicioRankingDinamico.ActualizarRanking(partidoDto);
+
+        var localActualizado = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == "Barcelona");
+        var visitanteActualizado = _equipoRepositorio.ObtenerEquipo(e => e.Nombre == "Bayern");
+        Assert.AreEqual(1523, localActualizado.RankingFifa);
+        Assert.AreEqual(1477, visitanteActualizado.RankingFifa);
+    }
+
 }
