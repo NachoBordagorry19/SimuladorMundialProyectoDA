345ce97de930b49679a16df445e15fd74db7a162
Nacho <ignaciorubino14@gmail.com>
2026-06-08 14:20:26 -0300

    [RED] CrearNotificacion()

--- WorldCupPlanner/TestDominio/NotificacionTest.cs
+++ WorldCupPlanner/TestDominio/NotificacionTest.cs
@@ -1,6 +1,15 @@
+using Dominio.Clases;
+
 namespace TestDominio;
 
+[TestClass]
 public class NotificacionTest
 {
-    
+    [TestMethod]
+    public void CrearNotificacion()
+    {
+        DateTime fechaHora = DateTime.Now;
+        Usuario usuario = new Usuario();
+        Notificacion not = new Notificacion("Mensaje", fechaHora, usuario.Id);
+    }
 }
