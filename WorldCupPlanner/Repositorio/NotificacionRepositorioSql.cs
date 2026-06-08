48975e6ce6a4a964170e2cc73f36e7cf5dd7a9e8
Nacho <ignaciorubino14@gmail.com>
2026-06-08 16:16:55 -0300

    Se crea NotificacionRepositorioSql y se implementa lo minimo para que o de error

---
+++ WorldCupPlanner/Repositorio/NotificacionRepositorioSql.cs
@@ -0,0 +1,22 @@
+using Dominio.Clases;
+using Repositorio.Interfaces;
+
+namespace Repositorio;
+
+public class NotificacionRepositorioSql : INotificacionRepositorio
+{
+    public void AgregarNotificacion(Notificacion not)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarcarComoLeida(int notId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Notificacion> ObtenerNotificacionesNoLeidas(int usuarioId)
+    {
+        throw new NotImplementedException();
+    }
+}
