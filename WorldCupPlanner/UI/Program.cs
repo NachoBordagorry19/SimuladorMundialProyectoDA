20d5454cadf4a204ae8e706b1e087034b5e0dcbe
Nacho <ignaciorubino14@gmail.com>
2026-06-10 18:23:50 -0300

    Se agrega INotificacionRepositorio y IServicioNotificacion a Program.cs

--- WorldCupPlanner/UI/Program.cs
+++ WorldCupPlanner/UI/Program.cs
@@ -40,6 +40,8 @@ builder.Services.AddScoped<IServicioPartido, PartidoServicios>();
 builder.Services.AddScoped<IImportadorEquiposCSV, ImportadorEquiposCSV>();
 builder.Services.AddScoped<IServicioFixturePrimeraFase, FixturePrimeraFaseServicio>();
 builder.Services.AddScoped<IServicioCrucesSegundaFase, CrucesSegundaFaseServicio>();
+builder.Services.AddScoped<INotificacionRepositorio, NotificacionRepositorioSql>();
+builder.Services.AddScoped<IServicioNotificacion, ServicioNotificacion>();
 
 builder.Services.AddScoped<UsuarioSesion>();
 builder.Services.AddScoped<ISessionService, SessionService>();
