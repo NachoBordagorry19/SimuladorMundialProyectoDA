abc32073805499cc13aa451801735fd83d79b632
Fabrizio Perotti <fperotti2323@gmail.com>
2026-06-15 22:36:41 -0300

    Agrego test para cambio de nombre en ActualizarEstadio

--- WorldCupPlanner/TestServicios/EstadioServiciosTest.cs
+++ WorldCupPlanner/TestServicios/EstadioServiciosTest.cs
@@ -187,4 +187,23 @@ public class EstadioServiciosTest
         _estadioServicios.ActualizarEstadio(estadioEditado);
         _auditoriaMock.Verify(a => a.RegistrarEdicionEstadio(estadioEditado.Nombre), Times.Once);
     }
+
+    [TestMethod]
+    public void ActualizarEstadio_CambioNombre_ActualizaCorrectamente()
+    {
+        _estadioServicios.AgregarEstadio(_estadioDTO);
+
+        EstadioDTO estadioNombreNuevo = new EstadioDTO()
+        {
+            Nombre = "CDS Nuevo",
+            Ciudad = _estadioDTO.Ciudad,
+            Descripcion = _estadioDTO.Descripcion,
+            CapacidadLocativa = _estadioDTO.CapacidadLocativa
+        };
+
+        _estadioServicios.ActualizarEstadio("CDS", estadioNombreNuevo);
+        EstadioDTO obtenido = _estadioServicios.ObtenerEstadioPorNombre("CDS Nuevo");
+
+        Assert.AreEqual("CDS Nuevo", obtenido.Nombre);
+    }
 }
